cd53ca04729aa1e265c512c2409d211c45e4aae3
Kafil Nasdami <kafil.nasdami@cegedim.com>
2019-06-19 00:41:16 +0100

    Updates

---
+++ precache-manifest.e1fa040efba6b8c481e4f999233208ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbb14f077b757c70898d24148da1cefd",
+    "url": "/static/media/bg.bbb14f07.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ca1f8a87e4d0721ae3c",
+    "url": "/static/js/main.119fc26f.chunk.js"
+  },
+  {
+    "revision": "2eb67b18693a4bbecf62",
+    "url": "/static/js/2.ebada9e9.chunk.js"
+  },
+  {
+    "revision": "9ca1f8a87e4d0721ae3c",
+    "url": "/static/css/main.6458eb43.chunk.css"
+  },
+  {
+    "revision": "2eb67b18693a4bbecf62",
+    "url": "/static/css/2.510a1acd.chunk.css"
+  },
+  {
+    "revision": "011c84053a4de01b6cea36e09fadabcd",
+    "url": "/index.html"
+  }
+];
