fe4b6e989955e196edde8bf99a266d6605ac79ea
Kafil NASDAMI <kafil.nasdami@gmail.com>
2019-07-05 22:27:28 +0100

    Updates

---
+++ precache-manifest.add34a474912b7fc16607209215f43f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f3438276d3207b98ff8",
+    "url": "/static/css/main.367402ca.chunk.css"
+  },
+  {
+    "revision": "5f3438276d3207b98ff8",
+    "url": "/static/js/main.00017e56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5354cd5708ffc3a952f4",
+    "url": "/static/css/2.babcf605.chunk.css"
+  },
+  {
+    "revision": "5354cd5708ffc3a952f4",
+    "url": "/static/js/2.788e060b.chunk.js"
+  },
+  {
+    "revision": "bbb14f077b757c70898d24148da1cefd",
+    "url": "/static/media/bg.bbb14f07.png"
+  },
+  {
+    "revision": "ba0bbb9f81a994ca713e791e0a95b9df",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76ff3bd1680ff5e06ab2c428c5260e81.js"
+  "/precache-manifest.add34a474912b7fc16607209215f43f0.js"
 );
 
 workbox.clientsClaim();
