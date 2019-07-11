36b09f4625ba73f4e1e0bf3caab66cb038f6fe98
Kafil Nasdami <kafil.nasdami@cegedim.com>
2019-07-11 14:42:27 +0100

    POC PDF

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from 'react';
 import 'reset-css';
 import './App.css';
 import moment from 'moment';
+import {
+  PDFDownloadLink,
+  Page,
+  Font,
+  Text,
+  View,
+  Document,
+  StyleSheet
+} from '@react-pdf/renderer';
 
 import Spinner from './common/spinner';
 import PrayerCard from './components/prayerCard';
@@ -13,6 +22,44 @@ import {
 } from './utils/localStorage';
 
 const API_URL = 'https://maroc-salat.herokuapp.com/';
+const NAMES = require('./data/prayers')
+
+// Register font
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf'
+});
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+    backgroundImage:'url(img/bg.png) no-repeat top center',
+    fontFamily: 'Oswald'
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1
+  }
+});
+
+// Create Document Component
+const MyDocument = ({prayer}) => (
+  
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Prayers times in {prayer.city}</Text>
+        <Text>{moment(prayer.date).format('dddd LL')}</Text>
+      {NAMES.map(e=>{
+        return <Text>{e} : {prayer[e]}</Text>
+      })}
+
+      </View>
+    </Page>
+  </Document>
+);
 
 const App = () => {
   const [id, setId] = useLocalStorage('id', 1);
@@ -36,6 +83,10 @@ const App = () => {
     init();
   }, []);
 
+  // const downloadPdf = () => {
+  //   ReactPDF.render(<MyDocument />, `example.pdf`);
+  // };
+
   return (
     <div id="main">
       {id && prayers ? (
@@ -46,6 +97,13 @@ const App = () => {
             values={cities}
             onChange={e => setId(e.value)}
           />
+          <div>
+            <PDFDownloadLink document={<MyDocument prayer={prayers.find(e => e.id === id)} />} fileName={`${moment()}.pdf`}>
+              {({ blob, url, loading, error }) =>
+                loading ? 'Loading document...' : 'Download now!'
+              }
+            </PDFDownloadLink>
+          </div>
         </>
       ) : (
         <Spinner />
