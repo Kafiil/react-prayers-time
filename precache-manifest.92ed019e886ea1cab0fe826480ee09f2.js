84a899e1db4eaa0881ccd2247aaa713347258539
Kafil <kafil@stajnari-ltm8.internal.salesforce.com>
2019-08-11 14:30:59 +0100

    Updates

---
+++ precache-manifest.92ed019e886ea1cab0fe826480ee09f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80039825f216f794d54ad4d0a9527c3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ed6d9d9441c1688f5d",
+    "url": "/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "293bf9269f1145315087",
+    "url": "/static/css/main.567e2439.chunk.css"
+  },
+  {
+    "revision": "f3ed6d9d9441c1688f5d",
+    "url": "/static/js/2.d2e34109.chunk.js"
+  },
+  {
+    "revision": "293bf9269f1145315087",
+    "url": "/static/js/main.cdfb3727.chunk.js"
+  },
+  {
+    "revision": "042c9098f8bc42df5e95",
+    "url": "/static/js/runtime~main.e1067735.js"
+  }
+]);
